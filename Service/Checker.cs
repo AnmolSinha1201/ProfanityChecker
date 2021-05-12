08641e64c7d8aaf08a0cc7c2340f88cc99643ee7
Anmol Sinha <anmolsinha1201@outlook.com>
2021-05-12 15:57:20 +0300

    Added checker service

---
+++ Service/Checker.cs
@@ -0,0 +1,54 @@
+using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading;
+
+namespace Service
+{
+	public static class ProfanityChecker
+	{
+		public static Dictionary<string, IndividualStatistic> WordDictionary = new();
+		public static WordListStatistic OverallStat = new();
+		public static string SourceFile = "ProfaneWords.txt";
+		static object Locker = new object();
+
+		static ProfanityChecker()
+		{
+			if (!File.Exists(SourceFile))
+			{
+				Console.WriteLine($"{SourceFile} DOES NOT EXIST");
+				return;
+			}
+
+			WordDictionary = File.ReadAllLines(SourceFile)
+				.Where(i => !i.Trim().Equals(""))
+				.ToDictionary(k => k, v => new IndividualStatistic());
+		}
+
+		public static List<string> Check(string Sentence)
+		{
+			var foundList = new List<string>();
+			OverallStat.Frequency++;
+
+			var watch = new Stopwatch();
+			watch.Start();
+			foreach (var word in Sentence.Split(' ', StringSplitOptions.RemoveEmptyEntries))
+			{
+				if (!WordDictionary.ContainsKey(word))
+					continue;
+
+				WordDictionary[word].Frequency++;
+				foundList.Add(word);
+			}
+			watch.Stop();
+			lock(Locker)
+			{
+				OverallStat.TotalTime.Add(watch.Elapsed);
+			}
+
+			return foundList;
+		}
+	}
+}
