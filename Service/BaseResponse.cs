0e58882c320924b09a94a76381bdd880a7087eef
Anmol Sinha <anmolsinha1201@outlook.com>
2021-05-12 23:59:12 +0300

    Added autostat pattern

--- Service/BaseResponse.cs
+++ Service/BaseResponse.cs
@@ -1,3 +1,8 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Newtonsoft.Json;
+
 namespace Service
 {
 	public class Successful : BaseResponse
@@ -9,10 +14,47 @@ namespace Service
 	public abstract class BaseResponse
 	{
 		public string Description;
+		public string Status
+		{
+			get 
+			{
+				var attributes = this.GetType()
+					.GetCustomAttributes(typeof(StatusText), true)
+					.Cast<StatusText>()
+					.ToList();
+
+				if (attributes.Count > 0)
+					return attributes.First().Text;
+
+				return this.GetType().Name;
+			}
+		}
 
 		public override string ToString()
-		{
-			return $"{{ \"Status\" : \"{this.GetType().Name}\", \"Description\" : \"{this.Description}\" }}"; 
-		}
+		=> JsonConvert.SerializeObject(this, new JsonSerializerSettings { NullValueHandling = NullValueHandling.Ignore });
+	}
+
+	[StatusText("Success")]
+	public class CheckResultSuccess : BaseResponse
+	{
+		public List<string> WordList = new();
+		public TimeSpan TimeTaken = new();
+	}
+
+	[AttributeUsage(AttributeTargets.Class, AllowMultiple = false)]
+	public class StatusText : Attribute
+	{
+		public string Text;
+
+		public StatusText(string Text)
+		=> this.Text = Text;
+	}
+
+	[StatusText("Success")]
+	public class OverallStatisticSuccess : BaseResponse
+	{
+		public List<ProfaneWord> PopularWords { get; set; } = new();
+		public TimeSpan TotalTime { get; set; }
+		public int TimesCalled { get; set; }
 	}
 }
